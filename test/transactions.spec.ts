4c25dd58aeb4b2f2cfd762791cae930673fc153f
Daniel Pinna <danielsouzapinna@gmail.com>
2025-02-03 07:49:16 -0300

    chore: to categorize a tests and add test list all transactions

---
+++ test/transactions.spec.ts
@@ -0,0 +1,48 @@
+import { afterAll, beforeAll, it, describe, expect } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Nova transacação',
+        amount: 23000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Nova transacação',
+        amount: 23000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') ?? []
+
+    const listTransactionsResponde = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponde.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Nova transacação',
+        amount: 23000,
+      }),
+    ])
+  })
+})
